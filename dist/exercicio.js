6186875a9a90063109cc5e7f98870fbd2b233da8
calopsitaanderson <calopsitaanderson@gmail.com>
2019-08-07 22:00:03 -0300

    exercicio de media de alunos

---
+++ dist/exercicio.js
@@ -0,0 +1,9 @@
+var nota1 = 10;
+var nota2 = 9;
+var nota3 = 5;
+var media = (nota1 + nota2 + nota3) / 3;
+console.log("sua media é " + media);
+if (media >= 7)
+    console.log("aprovado");
+else
+    console.log("reprovado");
